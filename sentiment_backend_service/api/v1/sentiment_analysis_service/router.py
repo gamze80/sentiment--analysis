4cb43906d4b83182692e5f46d87eeb6086619bf0
gamze80 <73304412+gamze80@users.noreply.github.com>
2022-07-06 17:26:39 +0300

    Update router.py

--- sentiment_backend_service/api/v1/sentiment_analysis_service/router.py
+++ sentiment_backend_service/api/v1/sentiment_analysis_service/router.py
@@ -1,6 +1,6 @@
 from sentiment_backend_service.interfaces.generic_router import GenericRouter
 from sentiment_backend_service.models.analysis import SentimentRequest, SentimentResponse
-from .repository import AnalysisRepository
+from . import repository
 from . import test
 
 
@@ -10,6 +10,7 @@ class AnalysisRouter(GenericRouter):
         self.bind_routes()
 
     def bind_routes(self):
+        self.get_router().get("/model_download", tags=["Predictions"])(self.download_model)
         self.get_router().post("/predict_text", response_model=SentimentResponse, tags=["Predictions"])(self.predict)
         self.get_router().post("/predict_list", tags=["Predictions"])(
             self.request_list)
@@ -17,7 +18,10 @@ class AnalysisRouter(GenericRouter):
 
     def predict(self,
                 request: SentimentRequest):
-        return AnalysisRepository.predict(request)
+        return repository.predict(request)
 
     def request_list(self):
-        return AnalysisRepository.request_list(None)
+        return repository.request_list()
+
+    def download_model(self):
+        return repository.download()
