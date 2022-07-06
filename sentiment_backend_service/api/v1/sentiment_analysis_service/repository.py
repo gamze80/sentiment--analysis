cf63e8d38384cefeeaf6ec4d2a42230b45929be7
gamze80 <73304412+gamze80@users.noreply.github.com>
2022-07-06 17:27:45 +0300

    Update repository.py

--- sentiment_backend_service/api/v1/sentiment_analysis_service/repository.py
+++ sentiment_backend_service/api/v1/sentiment_analysis_service/repository.py
@@ -1,21 +1,30 @@
+import gdown
 import requests
+from sentiment_backend_service.settings import ip
 from sentiment_backend_service.api.v1.sentiment_analysis_service.sentiment_analyzer.model import model
 from sentiment_backend_service.models.analysis import SentimentRequest, SentimentResponse
-from sentiment_backend_service.settings import ip
 
 
-class AnalysisRepository:
-    def predict(request:SentimentRequest ):
+def request_list():
+    url = f"http://{ip}:8000/v1/sentiment_backend/analysis_service/comments"
+    r = requests.get(url=url)
+    data = r.json()
+    results = []
+    for i in data:
+        results.append(i)
+        sentiment, confidence, probabilities = model.predict(i['comment'])
+        i['prediction'] = SentimentResponse(sentiment=sentiment, confidence=confidence, probabilities=probabilities)
+    return results
+
+
+def download():
+    gdown.download(
+        "https://drive.google.com/uc?id=1V8itWtowCYnb2Bc9KlK9SxGff9WwmogA",
+        "sentiment_backend_service/api/v1/sentiment_analysis_service/assets/model_state_dict.bin",
+    )
+    return "Model downloaded successfully"
+
+
+def predict(request: SentimentRequest):
         sentiment, confidence, probabilities = model.predict(request.text)
         return SentimentResponse(sentiment=sentiment, confidence=confidence, probabilities=probabilities)
-
-    def request_list(x:None):
-        url = f"http://{ip}:8000/v1/sentiment_backend/analysis_service/comments"
-        r = requests.get(url=url)
-        data = r.json()
-        results = []
-        for i in data:
-            results.append(i)
-            sentiment, confidence, probabilities = model.predict(i['comment'])
-            i['prediction'] = SentimentResponse(sentiment=sentiment, confidence=confidence, probabilities=probabilities)
-        return results
